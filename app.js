98d0a59249d52bb2a2cc07fe7661beca18e9c841
A.Sailesh Padmanabhan <arjun_sailesh@yahoo.com>
2022-09-09 10:55:52 +0530

    app.js

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const routes = require('./routes')
+const port = process.env.PORT || 3001;
 app.use(express.json())
 app.use(cors())
 
 app.use("/item", routes)
-app.listen(5000, function(req, res) {
+app.listen(port, function(req, res) {
     console.log("server started")
 })
